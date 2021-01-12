94ca1e44f9e8464d5cf48355c567ca1488a2cd48
TonyXiao <tony.s.xiao@gmail.com>
2021-01-11 22:07:03 -0800

    start calc project

--- fundamentals_4_practice/javascript-exercises/calculator/calculator.js
+++ fundamentals_4_practice/javascript-exercises/calculator/calculator.js
@@ -1,25 +1,32 @@
-function add () {
-	
+function add (x, y) {
+	return x + y;
 }
 
-function subtract () {
-	
+function subtract (x, y) {
+	return x - y;
 }
 
-function sum () {
-	
+function sum (arr) {
+	if(arr.length === 0) return 0;
+	return arr.reduce((accu, curr)=>accu+curr);
 }
 
-function multiply () {
+function multiply (arr) {
 	
+	return arr.reduce((accu, curr)=>{return accu*curr});
 }
 
-function power() {
-	
+function power(x, y) {
+	if(y === 0) return 1;
+	let ans = x;
+	for(let i = 2; i <= y; ++i) {
+		ans *= x;
+	}
+	return ans;
 }
 
-function factorial() {
-	
+function factorial(x) {
+	return x <= 1 ? 1 : x * factorial(x-1);
 }
 
 module.exports = {
--- fundamentals_4_practice/javascript-exercises/palindromes/palindromes.js
+++ fundamentals_4_practice/javascript-exercises/palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function() {
-
+const palindromes = function(word) {
+    for(let i = 0; i <= Math.floor(word.length/2); ++i) {
+        if(word.charAt(i) != word.charAt(word.length - 1 - i))
+        return false;
+    }
+    return true;
 }
 
 module.exports = palindromes
