1a589824a7136134407346a1778755dbbbf93c62
TonyXiao <tony.s.xiao@gmail.com>
2021-01-26 21:43:00 -0800

    more tut

---
+++ webpack_demo/src/index.js
@@ -0,0 +1,13 @@
+import _ from 'lodash';
+import {myName} from './myName.js'
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = myName('tony');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
