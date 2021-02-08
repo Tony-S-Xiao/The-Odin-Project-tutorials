fa65a3d40c79b8db771ad6743c1c27002f71b4be
TonyXiao <tony.s.xiao@gmail.com>
2021-02-07 18:08:16 -0800

    changed to async

--- weather_app/src/index.js
+++ weather_app/src/index.js
@@ -3,13 +3,21 @@ import getCurrWeather from './model/weather.js';
 
 
 let button = document.querySelector('button');
-button.addEventListener('click', (e)=>{
-    let fetched = getCurrWeather(document.querySelector('input').value);
-    document.querySelector('input').value = "";
+button.addEventListener('click', async (e)=>{
+    try {
+        let fetched = await getCurrWeather(document.querySelector('input').value);
+        document.querySelector('input').value = "";
+        console.log(fetched);
+        addWeather(fetched.name, fetched.weather[0].main, fetched.main.temp);        
+    } catch (err) {
+        alertErr('city not found!');
+    }
+    /*
     fetched.then((result)=>{
     console.log(result);
     addWeather(result.name, result.weather[0].main, result.main.temp);
     }).catch((err)=>{alertErr(err)});
+    */
 })
 let input = document.querySelector('input');
 input.addEventListener('keydown', (e)=> {
--- weather_app/src/model/weather.js
+++ weather_app/src/model/weather.js
@@ -1,17 +1,9 @@
 import api_key from './key.js'
-const getCurrWeather = (city) => {
-        return new Promise((resolve, reject)=>{
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`)
-        .then((response)=>{
-            return response.json();
-        })
-        .then((value)=>{
-            if(value.cod == '404') {
-                reject('City not found!');
-            } else {
-                resolve(value);
-            }
-        });
-    });
+const getCurrWeather = async (city) => {
+
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&mode=metric`);
+        let result = await response.json();
+        return result;
+
 }
 export default getCurrWeather;
