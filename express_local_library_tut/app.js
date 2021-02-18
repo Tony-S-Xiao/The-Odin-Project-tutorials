f6a1ed3200daf30c0295eb147301c88e5162b716
TonyXiao <tony.s.xiao@gmail.com>
2021-02-18 15:42:52 -0800

    mongodb

--- express_local_library_tut/app.js
+++ express_local_library_tut/app.js
@@ -3,7 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var key = require('./keys.js');
+const db_url = `mongodb+srv://tony:${key}@cluster0.kuo2e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
