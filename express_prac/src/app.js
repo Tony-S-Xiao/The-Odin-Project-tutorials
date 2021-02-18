50bdf94651e5c0dbedd052fe403f5d6585591819
TonyXiao <tony.s.xiao@gmail.com>
2021-02-17 22:13:15 -0800

    mongodb

--- express_prac/src/app.js
+++ express_prac/src/app.js
@@ -7,8 +7,22 @@ app.get('/', (req, res) => {
     fs.readFile('./about-me.html', (err, data)=>{
         if(err)
         res.status(404).send('404');
+        res.status(200).set('Content-Type', 'text/html');
         res.send(data);
-        
+    })
+});
+app.get('/contact', (req, res) => {
+    fs.readFile('./contact.html', (err, data)=>{
+        if(err)
+        res.status(404).send('404');
+        res.status(200).set('Content-Type', 'text/html');
+        res.send(data);
+    })
+});
+
+app.get(/.*/, (req, res) => {
+    fs.readFile('./404.html', (err, data)=>{
+        res.status(404).set('Content-Type', 'text/html').send(data);
     })
 });
 app.listen(port, ()=>{console.log('wow')});
